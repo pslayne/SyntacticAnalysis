b75e4f31746148aeb40d2ff050ac26913954db00
Layne Silveira <laynepsilveira@gmail.com>
2023-05-18 18:30:18 -0300

    main - lendo o arquivo e salvando os tokens

---
+++ Main.java
@@ -0,0 +1,17 @@
+package SyntacticAnalysis;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        //inicia a classe LexicalAnalyzer e recebe o arquivo txt com o cod
+        String FilePath = "C://Users//Usuario//Desktop//materiais de aula//UFERSA - CC//8º semestre//Compiladores//3//SyntacticAnalysis//lexical_analysis_result.txt";
+        SyntacticAnalyser.readFile(FilePath);
+
+        
+
+    }
+}
